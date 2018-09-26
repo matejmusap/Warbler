f07f431ee08ac4d7c922560b4a94a6e3bae18e91
matejmusap <matej.musap@gmail.com>
2018-09-26 11:39:08 +0200

    first commit

---
+++ warbler-server/handlers/auth.js
@@ -0,0 +1,32 @@
+const db = require("../models");
+const jwt = require("jsonwebtoken");
+
+exports.signin = async function(req, res, next) {};
+exports.sigunp = async function(req, res, next) {
+    try {
+        let user = await db.User.create(req.body);
+        let { id, username, profilImageUrl } = user;
+        let token = jwt.sign (
+            {
+                id,
+                username,
+                profilImageUrl
+            },
+            process.env.SECRET_KEY
+        );
+        return res.status(200).json({
+            id,
+            username,
+            profilImageUrl,
+            token
+        });  
+    } catch(err) {
+        if(err.code === 11000) {
+            err.message = "Sorry, that username and/or email is taken";
+        }
+        return next({
+            status: 400,
+            message: err.message
+        });
+    }
+};
